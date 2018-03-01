4a0a83a196aaf7ba61b199a41467efa481c38688
webManche <36086965+webManche@users.noreply.github.com>
2018-03-01 21:41:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/favicon.ico" sizes="48x48" type="img/ico">
+        <link rel="canonical" href="https://jeremyweb.com/index.php" />
+	<meta charset="UTF-8">
+	<title>Jeremyweb - Веб студия</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<img class="header-logo" src="img/logo_.png" alt="">
+					<h2 class="logo_name">eremyweb</h2>
+				</div>
+				<div class="col-lg-7">
+					<nav class="headernav">
+						<ul class="main-nav">
+							<li><a href="#" class="hvr-underline-from-center go_to">Главная</a></li>
+							<li><a href="#service" class="hvr-underline-from-center go_to">Услуги</a></li>
+							<li><a href="#projects" class="hvr-underline-from-center go_to">Проекты</a></li>
+							<li><a href="#contacts" class="hvr-underline-from-center go_to">Контакты</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col-lg-7">
+			<div class="languagess">
+			<p class="lang_text">Язык:</p>
+        	<ul>
+          <li><a class="russ" title="Русский"></a></li>
+          <li class="hvr-bounce-in"><a href="#" class="engl" title="English"> </a></li>
+        </ul>   
+      </div>
+	</div>
+	<img src="" alt="">
+	<div class="col-lg-12">
+	<div class="undel"></div>
+	<h2 class="header-name">Воплоти в реальность</br>сайт своей мечты!</h2>
+	<div class="enderlin"></div>
+	<small>Мы верим в воплощение идей, которые имеют смысл</small>
+	</div>
+	</div>
+	</div>
+	</div>
+</header>
+	<section id="service" class="service">
+		<div class="container">
+			<div class="col-lg-12">
+			<h2 class="service-title">Услуги</h2>
+			<div class="enderlin-2"></div>
+			</div>
+			<div class="col-lg-12">
+				<div class="tolss"><i class="fas fa-code"></i></div>
+				<h1 class="tols-name">Разработка сайтов</h1>
+				<p class="tols-text">Landing Page</br>Коммерческие сайты</br>Корпоративные сайты</br>Информационные сайты</br>Интернет-магазины</br>Интернет-порталы</p>
+				<div class="funcc"><i class="fas fa-cogs"></i></div>
+				<h1 class="funcc-name">Функциональность</h1>
+				<p class="funcc-text">Эффективный сайт — это инструментальный способ</br>для решения бизнес-задач любой компании.</br>Наши услуги предоставят пользователю возможность</br>интерактивно взаимодействовать и совершать любые</br>целевые действия.</p>
+				<div class="adapt"><i class="fas fa-desktop"></i></div>
+				<div class="adaptt-tel"><i class="fas fa-mobile"></i></div>
+				</div>
+				<div class="col-lg-12">
+				<h1 class="adaptt-name">Адаптивность</h1>
+				<p class="adaptt-text">Наши сайты, адаптированы к экранам различных устройств,</br>тем самым обеспечивая беспрепятственную возможность</br>просмотра и доступа к содержанию. Нами применяется</br>техника адаптивного дизайна, что позволяет обеспечить</br>посетителям веб-сайта доступность и удобство использования.</p>
+				<div class="supp"><i class="far fa-life-ring"></i></div>
+				<h1 class="supp-name">Поддержка 24/7</h1>
+				<p class="supp-text">Мы осуществляем техническую и информационную поддержку сайта.</br>Техническая поддержка подразумевает контроль за работой ресурса и исправление ошибок, влияющих на корректность работы сайта</p>
+			</div>
+		</div>
+	</section>
+	<section id="projects" class="project">
+		<div class="container">
+			<div class="col-lg-12">
+			<h2 class="project-title">Проекты</h2>
+			<div class="enderlin-3"></div>
+			</div>
+			<div class="col-lg-12">
+			<div id="container">
+    	<div id="slideshow" class="cycle-slideshow"
+       data-cycle-fx = "fade"
+       data-cycle-speed = "600"
+       data-cycle-timeout = "3000"
+       data-cycle-next = "#next"
+       data-cycle-prev = "#prev"
+       data-cycle-manual-fx = "scrollHorz"
+       data-cycle-manual-speed = "400">
+      <img src="img/project/Odo.PNG" alt="" title="ОДО">
+      <img src="img/project/elect.PNG" alt="" title="Электроэнергетика">
+      <img src="img/project/geot.png" alt="">
+<!--         <img src="img/Beige%20Lake%20Mt.jpg" alt="">
+<img src="img/keys.jpg" alt=""> -->
+    </div>
+    <div id="pager"></div>
+    <img id="prev" src="img/prev1.svg"/>
+    <img id="next" src="img/next1.svg"/>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+			<!-- <a href="#">
+				<div class="project_list">
+					<img src="img/logo_color.png" alt="" width="160px" height="130px">
+					<div class="project_bottom">Отдел дополнительгого образования СПбГАСУ</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="project_list">
+					<img src="img/7d38396597fb0c0492a5352589e78464.jpg" alt="" width="160px" height="130px">
+					<div class="project_bottom">Building Information Modeling Technology</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="project_list">
+					<img src="img/Logotip.JPG" alt="" width="160px" height="130px">
+					<div class="project_bottom">Благотворительная группа "Рука Помощи"</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="project_list">
+					<img src="img/elec.jpg" alt="" width="160px" height="130px">
+					<div class="project_bottom">Электроэнергетика и электротехника СПбГАСУ</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="project_list">
+					<img src="img/geot.png" alt="" width="160px" height="130px">
+					<div class="project_bottom">Кафедра геотехники СПбГАСУ</div>
+				</div>
+			</a> -->
+			</div>
+			</section>
+			<section id="contacts" class="contact">
+			<div class="container">
+				<div class="col-lg-12">
+				<h2 class="contact-title">Контакты</h2>
+				<div class="enderlin-3"></div>
+				<small class="small_cont">Расскажите о вашем проекте, и мы подготовим коммерческое предложение</small>
+			<!-- 	<div class="form">
+			            <div id="sendmessage">Your message has been sent. Thank you!</div>
+			            <div id="errormessage"></div>
+			            <form action="" method="post" role="form" class="contactForm">
+			              <div class="form-group">
+			                <input type="text" name="name" class="form-control" id="name" placeholder="Имя" data-rule="minlen:4" data-msg="Пожалуйста, напишите своё имя" />
+			                <div class="validation"></div>
+			              </div>
+			              <div class="form-group">
+			                <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Пожалуйста, напишите адрес электронной почты" />
+			                <div class="validation"></div>
+			              </div>
+			              <div class="form-group">
+			              	<select name="subject" id="subject" class="form-control" data-rule="minlen:4" data-msg="Пожалуйста выберите услугу">
+			              		<option value disabled selected>Выберите услугу</option>
+			              		<option value="1">Landing Page</option>
+			              		<option value="2">Коммерческие сайты</option>
+			              	    <option value="3">Информационные сайты</option>
+			              		<option value="4">Корпоративные сайты</option>
+			              		<option value="5">Интернет-магазины</option>
+			              		<option value="6">Интернет-порталы</option>
+			              		<option value="7">Другое</option>
+			              	</select>
+			                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+			                <div class="validation"></div>
+			              </div>
+			              <div class="form-group">
+			                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Пожалуйста, напишите сообщение" placeholder="Сообщение"></textarea>
+			                <div class="validation"></div>
+			              </div>
+			              <div class="text-center"><button class="btn btn-sm btn-primary" type="submit">Отправить</button></div>
+			            </form>
+			          </div> -->
+
+
+<div class="c-form-bottom">
+		                    <form role="form" action="contact/contact.php" method="post">
+		                    	<div class="form-group">
+		                    		<label for="c-form-name">
+		                    			<span class="label-text">Name:</span> 
+		                    			<span class="contact-error"></span>
+		                    		</label>
+		                        	<input type="text" name="name" placeholder="Your name..." class="c-form-name form-control" id="c-form-name">
+		                        </div>
+		                    	<div class="form-group">
+		                    		<label for="c-form-email">
+		                    			<span class="label-text">Email:</span> 
+		                    			<span class="contact-error"></span>
+		                    		</label>
+		                        	<input type="text" name="email" placeholder="Your email address..." class="c-form-email form-control" id="c-form-email">
+		                        </div>
+		                        <div class="form-group">
+		                        	<label for="c-form-profession">
+		                    			<span class="label-text">Profession:</span> 
+		                    			<span class="contact-error"></span>
+		                    		</label>
+					                <select name="profession" class="c-form-profession form-control" id="c-form-profession">
+					                	<option value="Your profession...">Your profession...</option>
+					                	<option value="Web design">Web design</option>
+					                	<option value="SEO">SEO</option>
+					                	<option value="Marketing">Marketing</option>
+					                </select>
+								</div>
+		                        <div class="form-group">
+		                        	<label for="c-form-subject">
+		                    			<span class="label-text">Subject:</span> 
+		                    			<span class="contact-error"></span>
+		                    		</label>
+		                        	<input type="text" name="subject" placeholder="Message subject..." class="c-form-subject form-control" id="c-form-subject">
+		                        </div>
+		                        <div class="form-group">
+		                        	<label for="c-form-message">
+		                    			<span class="label-text">Message:</span> 
+		                    			<span class="contact-error"></span>
+		                    		</label>
+		                        	<textarea name="message" placeholder="Message text..." class="c-form-message form-control" id="c-form-message"></textarea>
+		                        </div>
+		                        <button type="submit" class="btn">Send message</button>
+		                    </form>
+	                    </div>
+    
+				</div>
+				</div>
+			</div>
+			</section>
+<footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright">
+            &copy; Copyright JeremyWeb. Все права защищены
+            </div>
+            <div class="social">
+            <a href="https://vk.me/macho.gangster"><i class="fab fa-vk"></i></a>
+            <a href="https://t.me/mr_Jeremy"><i class="fab fa-telegram-plane"></i></a>
+            <a href="https://www.facebook.com/MachoGangster"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://vk.com/macho.gangster"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+      </div>
+    </div>
+ </footer>
+ <a href="#" class="back-to-top"><i class="fas fa-chevron-up"></i></a>
+
+<!-- <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script> -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/cycle2.js"></script>
+<script src="js/scroll.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114507514-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-114507514-1');
+</script>
+<script src="js/contactform.js"></script>
+
+</body>
+</html>
---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1500); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+
+    // Back to top button
+  $(window).scroll(function() {
+
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+      
+  });
+  
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500);
+      return false;
+  });
+});
